731871e38cbe605a1f2a59171d2defabefb98eb4
Nik Tanner <150745455+N1k0405@users.noreply.github.com>
2024-09-25 16:01:15 -0400

    Update Music.py

--- Hit Song Prediction/Music.py
+++ Hit Song Prediction/Music.py
@@ -1,6 +1,6 @@
-
 import pandas as pd
 import numpy as np
+
 # for the Machine Learning
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings("ignore")
 
-# Data path containing song meta data.
+# Data path containing song metadata.
 data_path = './spotify_data_urls.csv'
 
 # reads the csv file and assigns it to a pandas DataFrame 
@@ -16,6 +16,7 @@ data = pd.read_csv(data_path)
 
 #Features of each song
 X = data[['danceability','energy','key','loudness','mode','speechiness','acousticness','instrumentalness','liveness','valence','tempo']]
+
 # Label is either 1 or 0 indicating whether the song is a "hit" or not
 y = data[['Label']]
 
@@ -24,6 +25,8 @@ X_train ,X_test ,y_train , y_test = train_test_split(X, y , test_size = 0.2)
 
 #Loading logistic regression model from sklearn
 model = LogisticRegression()
+
+#Fitting the model and calculating accuracy against the test data
 model.fit(X_train, y_train)
 accuracy = model.score(X_test, y_test)
 print(accuracy)
@@ -34,10 +37,11 @@ print(pd.DataFrame(list(zip(X, coef[0])), columns=['feature', 'coef']))
 
 
 #Testing the removal of certain features to see the impact on accuracy
-#Removing seemingly arbitrary features seem to still have slight impacts on overall accuracy of the model 
+#Removing seemingly arbitrary features seems to still have slight impacts on the overall accuracy of the model 
 X2 = data[['danceability','energy','speechiness','acousticness','instrumentalness','liveness','valence']]
 y2 = data[['Label']]
 X2_train, X2_test, y2_train, y2_test = train_test_split(X2,y2, test_size=0.2)
+
 
 model.fit(X2_train,y2_train)
 accuracy = model.score(X2_test,y2_test)
